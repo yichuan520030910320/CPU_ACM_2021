0d435f14a7e02868a939453481c9623ef4f2bcc6
yichuan~ <73766326+yichuan520030910320@users.noreply.github.com>
2021-11-29 11:06:37 +0800

    dcache pass sim but failed on fpga and i will roll back

--- riscv/test/test.c
+++ riscv/test/test.c
@@ -1,13 +1,13 @@
 #include "io.h"
-int gcd(int x, int y) {
-  if (x%y == 0) return y;
-  else return gcd(y, x%y);
+int a(){
+
+    return 8;
 }
 
 int main() {
-    outlln(gcd(10,1));
-    outlln(gcd(34986,3087));
-    outlln(gcd(2907,1539));
-
-    return 0;
+    
+    for(int i=0;i<9;i++){
+    outlln(i+4);}
+    
+    
 }
